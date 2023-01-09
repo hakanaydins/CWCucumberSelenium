b70a9358c34bcf7cb1a5ad69de229b85ccea83ea
hakanaydins <auratheseason@gmail.com>
2023-01-10 00:07:37 +0300

    10.01.2023

---
+++ src/test/java/runners/TagRunner.java
@@ -0,0 +1,38 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+// Sadece @smoke tagine sahip olan senaryolari calistiran runner
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "@smoke",
+        dryRun = false
+)
+
+        /*
+        "@tag" -> @tag ile etiketlenen senaryolar
+        "@tag1 and not @tag2" -> @tag1 ile etiketlenen senaryolar, @tag2 ile etkilenmeyen senaryolar
+        "@tag1 and @tag2" -> Her ikisi ile etiketlenen senaryolar @tag1 ve @tag2
+        "@tag1 or @tag2" -> @tag1 ya da @tag2 ile etiketlenen senaryolar
+        ​
+        */
+
+
+
+public class TagRunner {
+}
+
+
+
+
+/*
+
+dryRun : iki secenek var.
+dryRun = true; -> yazdigimizda testimizi calistirmadan sadece eksik adimlari bize verir.
+dryRun = false; -> yazdigimizda testlerimizi calistirir.
+ */
+
---
+++ src/test/java/stepdefinitions/FirstStepDefinition.java
@@ -0,0 +1,22 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FirstStepDefinition {
+    @Given("Ilk feature icin cucumber indirdim")
+    public void ilk_feature_icin_cucumber_indirdim() {
+        System.out.println("Given step i calisti");
+    }
+    @When("Senaryomu calistirdigimda")
+    public void senaryomu_calistirdigimda() {
+        System.out.println("When step i calisti");
+    }
+    @Then("Senaryomun calistigini konsolda gormeliyim")
+    public void senaryomun_calistigini_konsolda_gormeliyim() {
+        System.out.println("Then step i calisti");
+    }
+
+
+}
